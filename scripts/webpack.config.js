160ea5cd47a703c1d68adc642758cc377b9f5158
Sean Beck <spcbeck@gmail.com>
2021-06-08 22:43:38 -0400

    Upgrading postcss-loader

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -61,17 +61,18 @@ module.exports = {
           {
             loader: "postcss-loader",
             options: {
-              ident: "postcss", // https://webpack.js.org/guides/migrating/#complex-options
-              plugins: () => [
-                autoprefixer({
-                  browsers: [
-                    ">1%",
-                    "last 4 versions",
-                    "Firefox ESR",
-                    "not ie < 9" // React doesn't support IE8 anyway
-                  ]
-                })
-              ]
+              postcssOptions: {
+                plugins: [
+                  autoprefixer({
+                    browsers: [
+                      ">1%",
+                      "last 4 versions",
+                      "Firefox ESR",
+                      "not ie < 9" // React doesn't support IE8 anyway
+                    ]
+                  })
+                ]
+              }
             }
           },
           "sass-loader"
