33a3cf125c0af12db3b6e0ce8337b5b3ffdd11c7
Sean Beck <spcbeck@gmail.com>
2021-06-08 23:41:01 -0400

    Adding browserList key to package.json and removing from webpack config

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -63,14 +63,7 @@ module.exports = {
             options: {
               postcssOptions: {
                 plugins: [
-                  autoprefixer({
-                    browsers: [
-                      ">1%",
-                      "last 4 versions",
-                      "Firefox ESR",
-                      "not ie < 9" // React doesn't support IE8 anyway
-                    ]
-                  })
+                  autoprefixer()
                 ]
               }
             }
